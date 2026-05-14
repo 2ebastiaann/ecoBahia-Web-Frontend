0fc8fee96e0d85022daa3fa29956b76005db1d0f
2ebastiaann <2ebastiaann@gmail.com>
2026-05-14 17:51:57 -0500

    configuración entorno de producción

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,14 @@
 // environment.prod.ts (producción)
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://ecobahia.eleueleo.com/api',
-  PROF_API_BASE_URL: 'http://apirecoleccion.gonzaloandreslucio.com/api',
+
+  // ==========================================
+  // 🚀 BACKEND DE PRODUCCIÓN (RAILWAY)
+  // ==========================================
+  API_BASE_URL: 'https://ecobahia-backend-production.up.railway.app/api',
+  
+  // API_BASE_URL: 'http://localhost:3007/api', // (Opción local)
+
+  PROF_API_BASE_URL: 'https://apirecoleccion.gonzaloandreslucio.com/api',
   PERFIL_ID: 'd1344313-5afa-40aa-b604-545ed54bd91b'
 };
